1be2ac31cfedfc53da7a7670484216b5050f880a
sandeep12biswas <sandeep12.biswas@gmail.com>
2024-03-17 21:08:45 +0530

    Added few more statements

--- HelloPython.py
+++ HelloPython.py
@@ -1,3 +1,23 @@
-
+# Comments can be given in this way starting with # at the beginning
 
 print("Hello Python and hello PCAP")
+a = 50
+print(type(a))
+print(id(a))
+
+a = 100
+print(type(a))
+print(id(a))
+
+
+def __init__(self=None):
+    print(self)
+    print(type(self))
+
+
+__init__()
+
+strValue = "My name is Sandeep"
+
+postSplit = strValue.split(" ")
+print(postSplit)
