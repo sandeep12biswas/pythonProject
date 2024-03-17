515b8d0cbda225f38582b049afea93467c7dc59a
sandeep12biswas <sandeep12.biswas@gmail.com>
2024-03-17 23:18:06 +0530

    Added few more statements

--- HelloPython.py
+++ HelloPython.py
@@ -12,12 +12,28 @@ print(id(a))
 
 def __init__(self=None):
     print(self)
-    print(type(self))
 
 
 __init__()
 
 strValue = "My name is Sandeep"
 
-postSplit = strValue.split(" ")
+postSplit = strValue.split(" ") # This is same as strValue.split(), because it splits on white space by default
 print(postSplit)
+print(" postSplit variable type is", type(postSplit))
+print("The value postString split is {}".format(type(postSplit)))
+
+print(strValue.upper())
+print(strValue)
+
+# Sorting String values
+
+nameList = ["Sandeep", "Mishika", "Alpana" , "Mousumi"]
+#nameList.__setitem__(4,"Ashok")
+nameList.sort()
+sortedNameList = nameList
+print(f"Sorted Name comes like {sortedNameList}")
+
+myDisc = {}
+myDisc.__setitem__("name","Sandeep")
+print(myDisc)
