aca2aef85d676919e594db0b8f6eb8ea5e8c3e61
sandeep12biswas <sandeep12.biswas@gmail.com>
2024-07-16 14:18:03 +0530

    added for loop for dictionary unpacking

--- disctionary_test.py
+++ disctionary_test.py
@@ -5,7 +5,7 @@ Print Bill's salary from the dictionary
 
 """
 
-my_list = [{'Tom' : 20000, 'Bill' : 12000}, ['car', 'laptop', 'tv']]
+my_list = [{'Tom': 20000, 'Bill': 12000}, ['car', 'laptop', 'tv']]
 
 print_state = "Bill's salary is {}".format(my_list[0]['Bill'])
 
@@ -17,8 +17,7 @@ bill_salary = f"Bill's salary is {my_list[0].get('Bill')}"
 
 print(bill_salary)
 
-employee = {"sam" : 10, "maya": 20, "Mou" :30}
+employee = {"sam": 10, "maya": 20, "Mou": 30}
 
-for k,v in employee.items():
+for k, v in employee.items():
     print(f"Dictionary key is {k} and value is {v}")
-
